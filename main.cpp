5c06317c43b27588d6be7b10006b209edbce0f97
wt <jw842503970@outlook.com>
2022-03-23 23:37:39 +0800

    projection

--- main.cpp
+++ main.cpp
@@ -11,9 +11,12 @@
 
 #include <vector>
 
+#include "Data.h"
+
 static float screen_width = 640.0f;
 static float screen_height = 480.0f;
 static std::vector< std::vector<unsigned int>> screen_color_buffer(screen_width + 1, std::vector<unsigned int>(screen_height + 1, 0XFFFFFF));
+static std::vector< std::vector<int>> screen_depth_buffer(screen_width + 1, std::vector<int>(screen_height + 1, 2147483647));
 
 struct Point
 {
@@ -28,7 +31,7 @@ struct Point
 	unsigned color = 0xFFFFFF;
 	int r,g,b;
 };
-
+ 
 static std::vector<Point> point_buffer_;
 static std::vector<int> index_buffer_;
 
@@ -58,6 +61,36 @@ struct Vector3
 
 	float x, y, z;
 };
+
+struct Vector4
+{
+	Vector4(float x, float y, float z, float w) :x(x), y(y), z(z), w(w) {}
+	Vector4() :x(0.f), y(0.f), z(0.f), w(0.f) {}
+	Vector4 operator+(const Vector4& v1) {
+		Vector4 v;
+		v.x = this->x + v1.x;
+		v.y = this->y + v1.y;
+		v.z = this->z + v1.z;
+		v.w = this->w + v1.w;
+		return v;
+	}
+
+	Vector4 operator-(const Vector4& v1) {
+		Vector4 v;
+		v.x = this->x - v1.x;
+		v.y = this->y - v1.y;
+		v.z = this->z - v1.z;
+		v.w = this->w - v1.w;
+		return v;
+	}
+
+	float Dot(const Vector4& v1) {
+		return this->x * v1.x + this->y * v1.y + this->z * v1.z + this->w * v1.w;
+	}
+
+	float x, y, z, w;
+};
+
 //中点画线法
 void line1(int x1, int y1, int x2, int y2) {
 
@@ -127,6 +160,23 @@ void BarycentricInTriangle(Point p, Point a, Point b, Point c, float& u, float& 
 	u = 1 - w - v;
 }
 
+//投影
+Vector4 Projection(Vector4& p)
+{
+	//这里先写死一个近平面1，远平面1000，fov 60，纵横比4/3的平截头体
+	double cot_fov_2 = 1.732;
+	double cot_fov_2_aspect = 1.299;
+	double near_plane = 1.0f;
+	double far_plane = 1000.0f;
+
+	Vector4 new_p;
+	new_p.x = p.x * cot_fov_2_aspect;
+	new_p.y = p.y * cot_fov_2;
+	new_p.z = (far_plane * p.z - far_plane * near_plane) / (far_plane - near_plane);
+	new_p.w = p.z;
+	return new_p;
+}
+
 void DrawTriangle(Point pa, Point pb, Point pc) {
 	float aabb_x_min = std::max(0.f, std::min(std::min(pa.x, pb.x), pc.x));
 	float aabb_x_max = std::min(screen_width, std::max(std::max(pa.x, pb.x), pc.x));
@@ -141,6 +191,10 @@ void DrawTriangle(Point pa, Point pb, Point pc) {
 			BarycentricInTriangle(Point((float)i, (float)j, 0), pa, pb, pc, u, v, w);
 			if (u < 0 || v < 0 || w < 0)
 			{
+				continue;
+			}
+			int depth = u * (float)pa.z + v * (float)pb.z + w * (float)pc.z;
+			if (screen_depth_buffer[i][j] < depth) {
 				continue;
 			}
 			int red = u * (float)pa.r + v * (float)pb.r + w * (float)pc.r;
@@ -178,10 +232,19 @@ int main()
 	//		color[i][j] = 0X00FF00;
 	//	}
 	//}
-	point_buffer_.push_back(Point(100.0, 0.0, 0.0, 0x000000FF));
-	point_buffer_.push_back(Point(100.0, 400.0, 0.0, 0x00FF0000));
-	point_buffer_.push_back(Point(400.0, 0.0, 0.0, 0x0000FF00));
-	point_buffer_.push_back(Point(400.0, 400.0, 0.0, 0x000000FF));
+	point_buffer_.push_back(Point(100.0, 0.0, 700.0, 0x000000FF));
+	point_buffer_.push_back(Point(100.0, 400.0, 700.0, 0x00FF0000));
+	point_buffer_.push_back(Point(400.0, 0.0, 700.0, 0x0000FF00));
+	point_buffer_.push_back(Point(400.0, 400.0, 700.0, 0x000000FF));
+	for (int i = 0; i < point_buffer_.size(); i++)
+	{
+		Vector4 point = Vector4(point_buffer_[i].x, point_buffer_[i].y, point_buffer_[i].z, 1.0f);
+		Vector4 point_after_projection = Projection(point);
+		double screen_x = (point_after_projection.x * screen_width / 2.0 / point_after_projection.w) + screen_width / 2;
+		double screen_y = (point_after_projection.y * screen_height / 2.0 / point_after_projection.w) + screen_height / 2;
+		double screen_z = point_after_projection.z / point_after_projection.w;
+		point_buffer_[i] = Point(screen_x, screen_y, screen_z, point_buffer_[i].color);
+	}
 	index_buffer_.push_back(0);
 	index_buffer_.push_back(1);
 	index_buffer_.push_back(2);
